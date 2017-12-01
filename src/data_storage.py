a119468c1c477a54b28f2b1eef93883f5620153a
MacStan <maciej.staniuk@gmail.com>
2017-12-01 19:39:06 +0100

    Moved plot generation to data_storage

--- src/data_storage.py
+++ src/data_storage.py
@@ -1,6 +1,8 @@
 import json
 import os
 import datetime as dt
+import sys
+from plotter import Plotter
 
 
 class DataStorage:
@@ -32,3 +34,42 @@ class DataStorage:
         with open(os.path.join(self.runStoragePath, f"seed{seedIndex}/data-point{dataPointIndex}"),
                   'r') as dataFile:
             return json.loads(dataFile.read())
+
+    def generate_images(self, new_path, seed, dataPointRange, freq, cords, distances):
+        print("Generating images!")
+
+        for dataPointIndex in range(0, dataPointRange):
+            self.plot_data_point(new_path,
+                                 self.get_net_configuration(seed),
+                                 self.get_data_point(seed, dataPointIndex),
+                                 dataPointIndex,
+                                 freq,
+                                 cords,
+                                 distances)
+            sys.stdout.write(f"Image {dataPointIndex} out of {dataPointRange}\r")
+        print("\nIt is done")
+
+    def get_map(self, acts, cords):
+        points = []
+        for pos in range(0, len(acts)):
+            for city in range(0, len(acts)):
+                if acts[city][pos] > 0.6:
+                    points.append(cords[city])
+
+        return points
+
+    def plot_data_point(self, new_path, net_conf, net_state, img_index, freq, cords, distances):
+        plotter = Plotter(6)
+        plotter.add_subplot(net_state["activations"], 'hot', 0, 1, f"Activations")
+        plotter.add_subplot(net_state["inputs"], 'coolwarm', -0.075, 0.075,
+                            f"Outputs of each neuron")
+        plotter.add_subplot(net_state["inputsChange"], 'Blues_r', -0.001, 0, f"Negative change")
+        plotter.add_subplot(net_state["inputsChange"], 'Reds', 0, 0.001, f"Positive change")
+        plotter.add_subplot(distances, 'plasma', 0, 1, f"Distance matrix")
+        plotter.add_graph(self.get_map(net_state["activations"], cords))
+        plotter.plot(
+            f"a {net_conf['a']}; b {net_conf['b']}; c {net_conf['c']}; "
+            f"d {net_conf['d']}; size_adj"
+            f" {net_conf['size_adj']}; u0 {net_conf['u0']}; "
+            f"timestep {net_conf['timestep']}; Step: {img_index * freq}",
+            f"{new_path}\img{img_index}.png")
--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
-from plotter import Plotter
-from hopfield import HopfieldNet
-from data_storage import DataStorage
-from input import distance_matrix, normalize, normalize_cords
-from pathlib import Path
-import subprocess as sp
 import datetime as dt
 import os
-import time
+import subprocess as sp
 import sys
+import time
+from pathlib import Path
+
+from data_storage import DataStorage
+from hopfield import HopfieldNet
+from input import distance_matrix, normalize, normalize_cords
 
 
 class RunParams:
@@ -20,48 +20,7 @@ class RunParams:
         self.tag = tag
 
 
-def generate_images(new_path, seed, dataPointRange, freq, cords, dataStorage, distances):
-    print("Generating images!")
-
-    for dataPointIndex in range(0, dataPointRange):
-        plot_data_point(new_path,
-                        dataStorage.get_net_configuration(seed),
-                        dataStorage.get_data_point(seed, dataPointIndex),
-                        dataPointIndex,
-                        freq,
-                        cords,
-                        distances)
-        sys.stdout.write(f"Image {dataPointIndex} out of {dataPointRange}\r")
-    print("\nIt is done")
-
-
-def get_map(acts, cords):
-    points = []
-    for pos in range(0, len(acts)):
-        for city in range(0, len(acts)):
-            if acts[city][pos] > 0.6:
-                points.append(cords[city])
-
-    return points
-
-
-def plot_data_point(new_path, net_conf, net_state, imgIndex, freq, cords, distances):
-    plotter = Plotter(6)
-    plotter.add_subplot(net_state["activations"], 'hot', 0, 1, f"Activations")
-    plotter.add_subplot(net_state["inputs"], 'coolwarm', -0.075, 0.075, f"Outputs of each neuron")
-    plotter.add_subplot(net_state["inputsChange"], 'Blues_r', -0.001, 0, f"Negative change")
-    plotter.add_subplot(net_state["inputsChange"], 'Reds', 0, 0.001, f"Positive change")
-    plotter.add_subplot(distances, 'plasma', 0, 1, f"Distance matrix")
-    plotter.add_graph(get_map(net_state["activations"], cords))
-    plotter.plot(
-        f"a {net_conf['a']}; b {net_conf['b']}; c {net_conf['c']}; "
-        f"d {net_conf['d']}; size_adj"
-        f" {net_conf['size_adj']}; u0 {net_conf['u0']}; "
-        f"timestep {net_conf['timestep']}; Step: {imgIndex * freq}",
-        f"{new_path}\img{imgIndex}.png")
-
-
-def run(params : RunParams):
+def run(params: RunParams):
     data_storage, net, new_path, normalized_distances = initialize(params)
 
     data_storage.start_new_seed(params.seed, net.get_net_configuration())
@@ -70,8 +29,10 @@ def run(params : RunParams):
     optimize_network(data_storage, params.freq, net, params.steps)
     print("\nAnnealing done!\n")
 
-    generate_images(new_path, params.seed, int(params.steps / params.freq), params.freq, normalize_cords(params.data), data_storage,
-                    normalized_distances)
+    data_storage.generate_images(new_path, params.seed, int(params.steps / params.freq),
+                                 params.freq,
+                                 normalize_cords(params.data),
+                                 normalized_distances)
 
     print("\nCreating video with ffmpeg")
     ffmpeg_command = f"ffmpeg -loglevel panic -r 10 -i {new_path}img%d.png -vframes {int(params.steps/params.freq)} " \
@@ -85,12 +46,12 @@ def run(params : RunParams):
         print(f"Video file created at: '{my_file}'")
     else:
         print("No video created :(")
-    print("Run Ended")
-    print()
+    print("Run Ended\n")
 
 
 def initialize(params):
-    print(f"Seed: {params.seed}; Steps: {params.steps}; Size_Adj: {params.size_adj}; Freq: {params.freq}")
+    print(
+        f"Seed: {params.seed}; Steps: {params.steps}; Size_Adj: {params.size_adj}; Freq: {params.freq}")
     normalized_distances = normalize(distance_matrix(params.data))
     data_storage = DataStorage()
 
